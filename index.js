5c49e63d8666bb036d7e45735525d8d66ec365c6
flawlesspaymentsinc <Tumeloyt@gmail.com>
2025-04-11 11:12:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 require('dotenv').config();
-
+const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
